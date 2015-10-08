8059003f6eca180bfff403052c9b4cf84c5c6dbe
DmytroKataryna94 <dmytro.kataryna@teamvoy.com>
2015-10-08 18:08:42 +0300

    srgseg

--- app/src/main/java/com/teamvoy/ci/citest/LoginActivity.java
+++ app/src/main/java/com/teamvoy/ci/citest/LoginActivity.java
@@ -54,12 +54,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      * fczsergsegbdrbd
-     * sghs
-     * egh
-     * se
-     * hs
-     * h
-     * se
      */
     private UserLoginTask mAuthTask = null;
 
