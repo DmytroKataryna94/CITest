93e3e51fa914cddf70c1e6cf65e74641f86a2d57
DmytroKataryna94 <dmytro.kataryna@teamvoy.com>
2015-10-08 16:17:43 +0300

    temp

--- app/src/androidTest/java/com/teamvoy/ci/citest/ApplicationTest.java
+++ app/src/androidTest/java/com/teamvoy/ci/citest/ApplicationTest.java
@@ -7,7 +7,12 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testPreconditions() {
+        assertNotNull("Activity is null", null);
     }
 }
