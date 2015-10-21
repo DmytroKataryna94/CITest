6d8aaf20bcede03cf9d556a62b2d30264cdd0f72
DmytroKataryna94 <dmytro.kataryna@teamvoy.com>
2015-10-21 17:45:59 +0300

    junit test conf

---
+++ app/src/androidTest/java/com/teamvoy/ci/citest/LoginActivityTest.java
@@ -0,0 +1,29 @@
+package com.teamvoy.ci.citest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by dmytro on 10/20/15.
+ */
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    private LoginActivity activity;
+
+    public LoginActivityTest() {
+        super(LoginActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        setActivityInitialTouchMode(false);
+        activity = getActivity();
+    }
+
+    public void testACt() {
+        assertNotNull("activity", activity);
+    }
+
+//    public void testACt2() {
+//        assertNull("activity", activity);
+//    }
+}
